0b5d81f4c38433b27689e18133a6d4f526626969
Sduncanpdx <62905755+Sduncanpdx@users.noreply.github.com>
2020-09-22 16:00:17 -0700

    barebones server with authentication

--- server/routes.py
+++ server/routes.py
@@ -1,6 +1,13 @@
+from .calling import get_projects
 from server import server
+
 
 @server.route('/')
 @server.route('/index')
 def index():
     return "hello world"
+
+@server.route('/all')
+def all():
+    projects = get_projects('sduncan', "Fuck0ffJama")
+    return "Number of Projects:" + str(projects)
