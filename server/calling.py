57b4f92a21e771f553e5f24943d0dab63aa3d34c
mirumcer <spencerduncan@spencers-mbp.lan>
2020-09-22 19:09:20 -0700

    test

--- server/calling.py
+++ server/calling.py
@@ -14,9 +14,12 @@ def get_projects(clientID, ClientPass):
     
     for project in project_list:
         project_name = project['fields']['name']
-        print('\n---------------' + project_name + '-----------------')
+        print('\n---------------' + project_name + '------------------')
+
+
 
     # Print each field
+    #
     for field_name, field_data in project.items():
 
         projects +=1
