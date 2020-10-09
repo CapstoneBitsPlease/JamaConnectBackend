78470a9138db0136407d3725a1b1446e56e5e628
KT Sutter <hermioninnyorjustaninny@gmail.com>
2020-10-08 21:30:13 -0700

    can add item to table and retrieve existing item by integer id

--- server/database.py
+++ server/database.py
@@ -1,5 +1,7 @@
 from flask import g
 import sqlite3
+from sqlite3 import Error
+from datetime import datetime
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -12,3 +14,56 @@ def query_db(query, args=(), one=False):
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
+
+def insert_into_db(table_name, id, title, type, service, linked_id, db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        c = conn.cursor()
+        c.execute("INSERT INTO "+table_name+" VALUES(?, ?, ?, ?, ?)", (id, title, type, service, linked_id))
+        conn.commit()
+        c.execute("SELECT * FROM "+table_name+" WHERE FieldID == '"+id+"'")
+        x = c.fetchall()
+        print("Got: ", x)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+def retrieve_by_id(table_name, id, db_file):
+    conn = None
+    row = None
+    try:
+        conn = sqlite3.connect(db_file)
+        c = conn.cursor()
+        c.execute("SELECT * FROM "+table_name+" WHERE ID == '"+id+"'")
+        row = c.fetchall()
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+    return row
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        c = conn.cursor()
+        c.execute("CREATE TABLE Fields(ITEM ID INT PRIMARY KEY, FieldID INT NOT NULL, LastUpdated DATETIME DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')), JiraName TEXT, JamaName TEXT);")
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+if __name__ == '__main__':
+    db_path = "/Users/ktsutter/Capstone/JamaConnectBackend/JamaJiraConnectDataBase.db"
+    #create_connection(db_path)
+    # Demo SELECT query
+    x = retrieve_by_id("Items", '1', db_path)
+    print("Retrieved from table: ", x)
+    time = datetime.now().strftime('%Y-%m-%d %H:%M:%f')
+    insert_into_db("Fields", '3', '3', time, 'Issue', 'Ticket', db_path)
