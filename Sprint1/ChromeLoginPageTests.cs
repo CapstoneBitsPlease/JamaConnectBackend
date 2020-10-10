c46456f39d6b34549583b4cbfbe67780dd31f59c
Brandon <bld@pdx.edu>
2020-10-10 12:45:52 -0700

    initial commit

---
+++ Sprint1/ChromeLoginPageTests.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace PSUCaptstoneTestingProject.Back_end
+{
+    public class GoogleChromLoginPageTests
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void A_SucessfullLoginTest()
+        {
+
+        }
+
+        [Test]
+        public void B_CorrectUsernameWrongPassword()
+        {
+
+        }
+
+        [Test]
+        public void C_WrongUsernameCorrectPassword()
+        {
+
+        }
+
+        [Test]
+        public void D_WrongUsernameWronPassword()
+        {
+
+        }
+    }
+}
