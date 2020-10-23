0bb895b17d5af798259bf7138ce70edff2853bbb
kt12359 <hermioninnyorjustaninny@gmail.com>
2020-10-22 17:53:00 -0700

    merged sprint2 into branch, updated get_items api call to use connections instead of functions get_session (doesn't exist anymore)

--- server/functions.py
+++ server/functions.py
@@ -4,28 +4,5 @@ import database
 import connections
 
 
-# function fo defining Jama and Jira user authentications
-# as well as session creation. 
-def authenticate_user(organization, username, password):
 
-    session = connections.cur_connections.new_connection()
-    
-    if(session.initiate_jama(organization, username, password, False) == "invalid"):
-        return "invalid"
-    
-    projects = session.jama_connection.get_projects()
-    #user = session.jama_connection.jama_client.g
-    print("user has been authenticated")
-    return session.id
-
-#give this function a session UUID and get the session object returned
-def get_session(token):
-    session = connections.cur_connections.get_session(token)
-    return session
-
-#gets the number of current users
-def get_cur_users():
-    connection_list = connections.cur_connections.all_connections
-    number = len(connection_list)
-    return {"number of users": number}
     
