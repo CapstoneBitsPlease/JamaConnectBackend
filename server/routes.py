31a50475b7627d5dfbec88470687ca833a429f62
Joel Sester <jsester@pdx.edu>
2020-11-02 20:28:14 -0800

    Try again

--- server/routes.py
+++ server/routes.py
@@ -296,5 +296,20 @@ def get_logs():
             error_list.append(error)
     return jsonify(error_list), 200
 
+@app.route('/post_link_item', methods=['POST'])
+def post_link_item():#returns false if the item called for does not exist in the Items table, does not check to see if the linked item exists in the items table
+    if request.method == 'POST':
+        input = request.values
+        id_primary=input["primary_ID"]#This is the id that remains the same whose linked_id is being updated
+        id_update=input["update_ID"]#this is the new linked_id
+        db_path = os.path.join(os.path.dirname(os.getcwd()), "JamaConnectBackend/JamaJiraConnectDataBase.db")
+        print(db_path)
+        itemsTableOps = ItemsTableOps(db_path)
+        databaseOperations = DatabaseOperations(db_path)
+        if databaseOperations.present_in_table("Items", id_primary):
+            itemsTableOps.update_linked_id(id_primary, id_update)
+            return jsonify("Success"), 200
+        return jsonify("The Item does not exist in the Items table"), 404
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
