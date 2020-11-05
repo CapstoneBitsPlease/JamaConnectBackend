3154d5d3f5f98acd1993a8c405ce0c9a5a3a2ee4
Bassel Hamieh <bahamieh@pdx.edu>
2020-11-05 15:32:33 -0800

    Updated get_jama_item_by_id and implements get_jira_item_by_id
    - Updated the get jama item by id functions with try and exception
    - Implemented the get jira item by id functions

--- server/connections.py
+++ server/connections.py
@@ -77,9 +77,19 @@ class connection:
             items.append(item)
         return items
 
-    def get_item_by_id(self, item_id):
-        response = self.jama_connection.get_item(item_id = item_id)
+    def get_jama_item_by_id(self, item_id):
+        try:
+            response = self.jama_connection.get_item(item_id = item_id)
+        except ResourceNotFoundException:
+            response = "Item ID not found."
         return response
+
+    def get_jira_item_by_id(self, key):
+        try:
+            response = self.jira_connection.issue(key = key)
+        except requests.exceptions.HTTPError:
+            response = "Item key not found."
+        return response    
         
     def match_token(self, token):
         if self.id == token:
