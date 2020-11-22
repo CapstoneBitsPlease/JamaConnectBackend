87de3b018af597df9ef32b929823e969344768fd
Joel Sester <jsester@pdx.edu>
2020-11-22 01:42:51 -0800

    fixed strptime

--- server/routes.py
+++ server/routes.py
@@ -404,16 +404,17 @@ def get_logs_range():
     start_date = input["start_date"]+"  12:00:00 AM"
     end_date = input["end_date"]+"  11:59:59 AM"
     form = '%m/%d/%Y %I:%M:%S %p'
-    start_time=datetime.strptime(start_date, form)
-    end_time=datetime.strptime(end_date, form)
+    start_time=datetime.datetime.strptime(start_date, form)
+    end_time=datetime.datetime.strptime(end_date, form)
     error_list = []
     with open('error_json.log') as logs:
         for json_obj in logs:
             error = json.loads(json_obj)
-            log_time=datetime.strptime(json_obj[asctime], form)
+            log_time=datetime.datetime.strptime(json_obj[asctime], form)
             if log_time>=start_time & log_time<=end_time:
                 error_list.append(error)
     return jsonify(error_list), 200
+
 
 # Links two items. Accepts 4 arrays: jama_item, jira_item, jama_fields, and jira_fields, and 1
 # integer parameter which indicates the total number of fields in each fields array.
