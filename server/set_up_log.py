d71510917d8c0adae354ba4fd7c8b272e86b357a
kt12359 <hermioninnyorjustaninny@gmail.com>
2020-11-29 17:32:23 -0800

    fixed multiple logging issue: the root logger automatically has a handler, and since I added a json handler it was using both. Solved by removing any handlers that exist when the root logger was retrieved.

--- server/set_up_log.py
+++ server/set_up_log.py
@@ -1,15 +1,10 @@
 import logging
 from pythonjsonlogger import jsonlogger
 
-def log_setup(level = logging.INFO):
-    root_logger= logging.getLogger()
-    root_logger.setLevel(level)
-    handler = logging.FileHandler('error.log', 'a', 'utf-8')
-    handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p'))
-    root_logger.addHandler(handler)
-
-def json_log_setup(level = logging.DEBUG):
+def json_log_setup(level = logging.INFO):
     logger = logging.getLogger()
+    if logger.hasHandlers():
+        logger.handlers.clear()
     logger.setLevel(level)
     logHandler = logging.FileHandler('error_json.log', 'a', 'utf-8')
     formatter = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s %(name)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
